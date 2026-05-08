65ccb37006e0118e68e4c14448c9369a3a48eea5
vrishtikohli <vrishtikohli@gmail.com>
2026-05-08 22:08:49 +0530

    Add program to print decreasing sequences

---
+++ 20b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 5;
+    for(int i = n; i >= 1; i--) {
+        for(int j = n; j >= n - i + 1; j--) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
