c4d9c7b9bfed3f367ab2ba4af2c372fa561389b3
vrishtikohli <vrishtikohli@gmail.com>
2026-05-08 22:44:51 +0530

    Refactor matrix transpose program by removing comments
    Removed comments and added newline at the end of the file.

---
+++ 35.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int matrix[3][3], transpose[3][3];
+    cout << "Enter elements of 3x3 matrix:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+    cout << "\nOriginal Matrix:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "\nTranspose Matrix:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << transpose[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
