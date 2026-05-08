216eb62caf86b3ba1b14b122471af8e5b1461326
vrishtikohli <vrishtikohli@gmail.com>
2026-05-08 22:19:16 +0530

    Add program to print alphabet pattern

---
+++ 24.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << char('A' + j);
+        }
+        cout << endl;
+    }
+    return 0;
+}
