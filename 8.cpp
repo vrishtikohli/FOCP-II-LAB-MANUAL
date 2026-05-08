c9d46e678e2137c77bd8e9d94c555e0426eed666
vrishtikohli <vrishtikohli@gmail.com>
2026-05-08 21:38:29 +0530

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            cout << "BuzzFuzz";
+        }
+        else if (i % 3 == 0) {
+            cout << "Buzz";
+        }
+        else if (i % 5 == 0) {
+            cout << "Fuzz";
+        }
+        else {
+            cout << i;
+        }
+        cout << endl;
+    }
+    return 0;
+}
